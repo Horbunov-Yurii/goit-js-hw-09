fe6fe72077b0855e88ed21789fb8682c6b45cb50
Horbunov Yurii <urijnet@gmail.com>
2023-07-27 22:20:24 +0300

    tasck-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  if (!timerId) {
+    startBtn.disabled = true;
+    timerId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  timerId = null;
+  startBtn.disabled = false;
+});
